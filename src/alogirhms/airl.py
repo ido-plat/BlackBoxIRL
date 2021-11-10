3035270809cf6bdc3743e04e00213e8ecbc259d5
Ido Yadid <idoy1508@gmail.com>
2021-11-10 12:18:17 +0200

    again

--- src/alogirhms/airl.py
+++ src/alogirhms/airl.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from src.utils.imitation_connector import to_im_traj
 from imitation.src.imitation.algorithms.adversarial import airl
 from imitation.src.imitation.data import rollout
@@ -6,18 +8,16 @@ import stable_baselines3 as sb3
 import tempfile
 import pathlib
 
-def airl(samples, venv):
-    trajectories = to_im_traj(samples)
-    transitions = rollout.flatten_trajectories(trajectories)
-    tempdir = tempfile.TemporaryDirectory(prefix="AIRL")
-    tempdir_path = pathlib.Path(tempdir.name)
-    airl_logger = logger.configure(tempdir_path / "AIRL/")
+def airl(samples, venv,policy_training_steps,total_timesteps , batch_size=32, logger=None):
+    if type(samples) == np.array:
+        samples = to_im_traj(samples)
+        samples = rollout.flatten_trajectories(samples)
     airl_trainer = airl.AIRL(
         venv=venv,
-        demonstrations=transitions,
-        demo_batch_size=32,
-        gen_algo=sb3.PPO("MlpPolicy", venv, verbose=1, n_steps=1024),
-        custom_logger=airl_logger,
+        demonstrations=samples,
+        demo_batch_size=batch_size,
+        gen_algo=sb3.PPO("MlpPolicy", venv, verbose=1, n_steps=policy_training_steps),
+        custom_logger=logger,
     )
-    airl_trainer.train(total_timesteps=2048)
-    return airl_trainer.reward_test()
+    airl_trainer.train(total_timesteps=total_timesteps)
+    return airl_trainer.reward_test().predict
